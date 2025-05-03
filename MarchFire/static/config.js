34902c85cf53011224a922a2f6b30300e24c11f0
keithmccaffery <132424932+keithmccaffery@users.noreply.github.com>
2025-05-04 08:25:26 +1000

    Updated capture*.html files to use centralized config.js for SAS token

---
+++ MarchFire/static/config.js
@@ -0,0 +1,5 @@
+const config = {
+    storageAccountName: 'firstfire',
+    containerName: 'firedoors1',
+    sasToken: "?sv=2024-11-04&ss=bfqt&srt=co&sp=rwdltfx&se=2026-05-03T17:38:49Z&st=2025-05-03T09:38:49Z&spr=https&sig=SHnehe1PvaMtgUOxMcVuTykObqXIefa%2FWbqL%2FzNZRNc%3D"
+};
